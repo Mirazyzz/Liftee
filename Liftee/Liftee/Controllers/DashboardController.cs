2018cbbf7a405e4e75e677be60d591cb3b192a28
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2022-09-16 10:42:50 +0500

    Add filtering through widgets

--- Liftee/Liftee/Controllers/DashboardController.cs
+++ Liftee/Liftee/Controllers/DashboardController.cs
@@ -15,7 +15,7 @@ namespace Liftee.Controllers
             _parser = new CsvParser();
         }
 
-        public IActionResult Index(ComboBoxFor model)
+        public IActionResult Index(string? category)
         {
             var totalProjects = ProjectsService.Projects.Count;
             ViewBag.TotalProjects = totalProjects;
@@ -36,59 +36,29 @@ namespace Liftee.Controllers
                     Count = k.Count()
                 }).ToList();
 
-            // New Projects
-            //var newProjects = ProjectsService.Projects.GroupBy(p => DateTime.Parse(p.InstallationStartDate).Month)
-            //    .Select(p => new SplineChartData()
-            //    {
-            //        month = DateTime.Parse(p.First().InstallationStartDate).ToString("MMM"),
-            //        newProjects = p.Count()
-            //    })
-            //    .ToList();
-
-            //var finishedProjects = ProjectsService.Projects.GroupBy(p => DateTime.Parse(p.InstallationFinishDate).Month)
-            //    .Select(p => new SplineChartData()
-            //    {
-            //        month = DateTime.Parse(p.First().InstallationFinishDate).ToString("MMM"),
-            //        finishedProjects = p.Count()
-            //    })
-            //    .ToList();
-
-            //var startDate = new DateTime(DateTime.Now.Year, 1, 1);
-
-            //string[] summary = Enumerable.Range(0, DateTime.Now.Month)
-            //    .Select(i => startDate.AddMonths(i).ToString("MMM"))
-            //    .ToArray();
-
-            //ViewBag.SplineChartData = from month in summary
-            //                          join newProject in newProjects on month equals newProject.month into newProjectMonthJoined
-            //                          from newProject in newProjectMonthJoined.DefaultIfEmpty()
-            //                          join finishedProject in finishedProjects on month equals finishedProject.month into finishedProjecMonthJoined
-            //                          from finishedProject in finishedProjecMonthJoined.DefaultIfEmpty()
-            //                          select new
-            //                          {
-            //                              month = month,
-            //                              newProjects = newProject == null ? 0 : newProject.newProjects,
-            //                              finishedProjects = finishedProject == null ? 0 : finishedProject.finishedProjects
-            //                          };
-
+            
             var filteredProject = ProjectsService.Projects;
 
-            if(model?.Val == "Законченные")
+            if (!string.IsNullOrEmpty(category))
             {
-                filteredProject = filteredProject.Where(p => p.ProjectStatus == "Закрыт").ToList();
+                if (category == "Finished")
+                {
+                    filteredProject = filteredProject.Where(p => p.ProjectStatus == "Закрыт").ToList();
+                }
+                else if (category == "InProccess")
+                {
+                    filteredProject = filteredProject.Where(p => p.ProjectStatus == "В процессе").ToList();
+                }
+                else if (category == "Problems")
+                {
+                    filteredProject = filteredProject.Where(p => p.Issues != "Нет" && !string.IsNullOrEmpty(p.Issues)).ToList();
+                }
+                else if (category == "All")
+                {
+                    filteredProject = ProjectsService.Projects;
+                }
             }
-            else if(model?.Val == "В процессе")
-            {
-                filteredProject = filteredProject.Where(p => p.ProjectStatus == "В процессе").ToList();
-            }
-            else if(model?.Val == "Проблемы")
-            {
-                filteredProject = filteredProject.Where(p => p.Issues != "Нет" && !string.IsNullOrEmpty(p.Issues)).ToList();
-            }
-            else if(model?.Val == "Все")
-            {
-                filteredProject = ProjectsService.Projects;
-            }
+
 
             ViewBag.Projects = filteredProject;
             ViewBag.ProjectsCount = filteredProject.Count;
